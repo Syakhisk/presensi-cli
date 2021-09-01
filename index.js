d2d516e7b410485a7a290f130cebd9785c6d9132
Syakhisk <mail.syakhisk@gmail.com>
2021-09-01 19:01:07 +0700

    moving to commander

--- index.js
+++ index.js
@@ -1,78 +1,62 @@
 require("dotenv").config()
+const nfzf = require( 'node-fzf' )
+const {bgCyan, red, cyan} = require('chalk')
 
-const puppeteer = require('puppeteer-core')
-const tempDir = require("temp-dir")
-const ora = require("ora")
-const chromePath = require("chrome-paths")
+const {access, readFile} = require('fs/promises')
+const inquirer = require('inquirer');
 
-const blockResources = require("./lib/helper-fn/blockResources.js")
-const waitAll = require("./lib/helper-fn/waitAll.js")
-const login = require("./lib/page-fn/login.js")
-const parseTatapMukaBunch = require("./lib/page-fn/parseTatapMukaBunch.js")
-const getTatapMukaToday = require("./lib/page-fn/getTatapMukaToday.js")
-
-const config = {
-  headless: true,
-  devtools:true,
-  executablePath: chromePath.chrome,
-  args: [`--user-data-dir=${tempDir}/pptr`]
-}
-
-const selectors = {
-  username: "#username",
-  password: "#password",
-  continue: "#continue",
-  login : "#login",
-  listKelas: ".block .block-content ul li .content-li",
-  itemKelas: "h5 a",
-  time: ".row .col",
-  tatapmuka_bunch: "table.js-table-sections-enabled tbody.js-table-sections-header",
+const opts = {
+  // list: [ 'whale', 'giraffe', 'monkey' ],
+  prefill: '',
+  prelinehook: function ( index ) { return '' },
+  postlinehook: function ( index ) { return '' }
 };
 
-const urls = {
-  myIts: "https://my.its.ac.id",
-  presensi: "https://presensi.its.ac.id",
-  kelasDummy: "https://presensi.its.ac.id/tatap-muka/20211:42100:ME184412:A"
-};
 
-(async () => {
-  const spinner = ora('Initiating...').start();
+(async function () {
+  const isClassesExists = await access("./user-classes.json").then(data => true).catch(() => false)
+  if(!isClassesExists) return false
 
-  const browser = await puppeteer.launch(config)
-  const page = await browser.newPage()
+  const userClasses = JSON.parse(await readFile("./user-classes.json", {encoding: "utf-8"}))
 
-  //* Login
-  await page.setRequestInterception(true);
-  page.on("request", blockResources)
 
-  //* Visit
-  spinner.text = `Heading to ${urls.presensi}`
-  await page.goto(urls.presensi, {waitUntil: 'domcontentloaded'});
+//   console.log("------------")
+//   console.log(bgCyan.black(" Presensi CLI "))
+//   console.log("------------")
+//   console.log()
+// 
+//   console.log(cyan("Choose a class: "))
+// 
+//   opts.list = userClasses.map(item => item.name)
+//   const result = await nfzf(opts)
+//   const { selected, query } = result
+// 
+//   if( !selected ) {
+//     console.log( 'No matches for:', query )
+//   } else {
+//     console.log( selected.value ) // 'giraffe'
+//     console.log( selected.index ) // 1
+//     console.log( selected.value === opts.list[ selected.index ] ) // true
+//   }
 
-  //* Login check
-  if(page.url().includes(urls.myIts)){
-    spinner.text = "Logging in"
-    await waitAll(
-      page.evaluate(login, {selectors, env: process.env}),
-      page.waitForNavigation({ waitUntil: "networkidle2" }),
-    )
-  } else {
-    spinner.text = "Logged in."
-  }
-
-  await page.goto(urls.kelasDummy, {waitUntil: 'domcontentloaded'})
-  await page.waitForSelector(selectors.tatapmuka_bunch)
-
-  spinner.text = "Getting tatap muka"
-  const listTatapMuka = await page.$$eval(selectors.tatapmuka_bunch, parseTatapMukaBunch).catch(e => console.log(e))
-
-  spinner.text = "Getting latest class"
-  const tatapMukaToday = getTatapMukaToday(listTatapMuka)
-  spinner.stop()
-
-  // console.log(listTatapMuka)
-  console.log(tatapMukaToday)
-
-  await browser.close()
-})();
-
+ inquirer
+  .prompt([
+    {
+      type: 'rawlist',
+      name: 'theme',
+      message: 'What do you want to do?',
+      pageSize: userClasses.length + 1,
+      choices: userClasses.map(item => item.name)
+    },
+  ])
+  .then((answers) => {
+  console.log(answers)
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("tty")
+    } else {
+    console.log(error)
+    }
+  });
+})()
--- presence.js
+++ presence.js
@@ -4,16 +4,17 @@ const puppeteer = require('puppeteer-core')
 const tempDir = require("temp-dir")
 const chromePath = require("chrome-paths")
 
-const filterKelas = require("./lib/page-fn/filterKelas.js")
-const login = require("./lib/page-fn/login.js")
 const blockResources = require("./lib/helper-fn/blockResources.js")
 const waitAll = require("./lib/helper-fn/waitAll.js")
-const {access, readFile, writeFile} = require('fs/promises')
+const login = require("./lib/page-fn/login.js")
+const parseTatapMukaBunch = require("./lib/page-fn/parseTatapMukaBunch.js")
+const getTatapMukaToday = require("./lib/page-fn/getTatapMukaToday.js")
 
 const config = {
   headless: true,
   devtools:true,
-  executablePath: chromePath.chrome, args: [`--user-data-dir=${tempDir}/pptr`]
+  executablePath: chromePath.chrome,
+  args: [`--user-data-dir=${tempDir}/pptr`]
 }
 
 const selectors = {
@@ -24,19 +25,18 @@ const selectors = {
   listKelas: ".block .block-content ul li .content-li",
   itemKelas: "h5 a",
   time: ".row .col",
-
+  tatapmuka_bunch: "table.js-table-sections-enabled tbody.js-table-sections-header",
 };
 
 const urls = {
   myIts: "https://my.its.ac.id",
   presensi: "https://presensi.its.ac.id",
+  kelasDummy: "https://presensi.its.ac.id/tatap-muka/20211:42100:ME184412:A"
 };
 
 (async () => {
   const ora = (await import("ora")).default
   const spinner = ora('Initiating...').start();
-
-  // const userClasses = await getUserClasses()
 
   const browser = await puppeteer.launch(config)
   const page = await browser.newPage()
@@ -47,13 +47,10 @@ const urls = {
 
   //* Visit
   spinner.text = `Heading to ${urls.presensi}`
-  await waitAll(
-    page.goto(urls.presensi),
-    page.waitForNavigation({waitUntil: 'domcontentloaded'})
-  )
+  await page.goto(urls.presensi, {waitUntil: 'domcontentloaded'});
 
   //* Login check
-  if(page.url().includes("my.its.ac.id")){
+  if(page.url().includes(urls.myIts)){
     spinner.text = "Logging in"
     await waitAll(
       page.evaluate(login, {selectors, env: process.env}),
@@ -63,20 +60,19 @@ const urls = {
     spinner.text = "Logged in."
   }
 
-  //* Get class
-  spinner.text = "Getting classes"
-  const listKelas = await page.$$eval(selectors.listKelas, filterKelas, selectors).catch((e) => {spinner.text = e})
+  await page.goto(urls.kelasDummy, {waitUntil: 'domcontentloaded'})
+  await page.waitForSelector(selectors.tatapmuka_bunch)
 
-  spinner.text = "Writing user-classes.json"
-  await writeFile("./user-classes.json", JSON.stringify(listKelas, null, 2), {encoding: "utf-8"})
+  spinner.text = "Getting tatap muka"
+  const listTatapMuka = await page.$$eval(selectors.tatapmuka_bunch, parseTatapMukaBunch).catch(e => console.log(e))
+
+  spinner.text = "Getting latest class"
+  const tatapMukaToday = getTatapMukaToday(listTatapMuka)
   spinner.stop()
-  console.log("User Classes written in " + __dirname + "/user-classes.json")
+
+  // console.log(listTatapMuka)
+  console.log(tatapMukaToday)
 
   await browser.close()
 })();
 
-async function getUserClasses(){
-  const isExist = await access("./user-classes.json").then(() => true).catch(() => false)
-  if(!isExists) return false
-  return JSON.parse(await readFile("./user-classes.json"))
-}
